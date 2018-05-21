c6e9de8ae407ddbf12f2ac0dd530a215a70e81c0
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2018-05-21 19:35:30 -0300

    Add questao_4.py
    First commit in Recife
    Thanks father Tom for the builtin sort method

--- questoes/questao_4.py
+++ questoes/questao_4.py
@@ -1,6 +1,6 @@
 ## QUESTÃO 4 ##
 #
-# Você foi solicitado a escrever um programa para classificar tuplas (nome, idade, altura) 
+# Você foi solicitado a escrever um programa para classificar tuplas (nome, idade, altura)
 # por ordem crescente, em que nome é uma string, idade e altura são números. As tuplas
 # são inseridas pelo console. O critério de classificação é:
 #
@@ -9,7 +9,7 @@
 # 3: Em seguida, classifique por pontuação.
 #
 # A prioridade é essa nome > idade > pontuação.
-# 
+#
 # Se as seguintes tuplas são dadas como entrada para o programa:
 # Tom 19,80
 # João, 20,90
@@ -21,22 +21,27 @@
 # [('John', '20', '90'), ('Jony', '17', '91'), ('Jony', '17', '93'), ('Json', '21 ',' 85 '), (' Tom ',' 19 ',' 80 ')]
 #
 # Dicas:
-# As entradas devem ser lidas do console até que um valor vazio seja digitado. 
+# As entradas devem ser lidas do console até que um valor vazio seja digitado.
 # A saída deve ser impressa no formato apresentado acima.
 # Valores inválidos (fora do formato esperado para uma tupla) devem ser descartados.
 ##
 
 
 ##
-# A sua resposta da questão deve ser desenvolvida dentro da função main()!!! 
+# A sua resposta da questão deve ser desenvolvida dentro da função main()!!!
 # Deve-se substituir o comado print existente pelo código da solução.
 # Para a correta execução do programa, a estrutura atual deve ser mantida,
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 4")
+    tuple_list = []
+    user_input = input().split(',')
+    while(user_input != ['']):
+        tuple_list.append(tuple(user_input))
+        user_input = input().split(',')
+    tuple_list.sort()
+    print(tuple_list)
 
 
-    
 if __name__ == '__main__':
     main()
